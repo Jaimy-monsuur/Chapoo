73ebe851be425128a4792a291617249f3d47c733
Bastiaan27 <70689135+Bastiaan27@users.noreply.github.com>
2021-05-24 23:03:43 +0200

    Order aangepast

--- ChapooUI/KeukenBarSysteemForm.Designer.cs
+++ ChapooUI/KeukenBarSysteemForm.Designer.cs
@@ -31,19 +31,21 @@ namespace ChapooUI
         {
             this.terugKeukenBarBtn = new System.Windows.Forms.Button();
             this.listViewKeukenBarOpenstaand = new System.Windows.Forms.ListView();
-            this.listViewKeukenBarIngredientenOpmerkingen = new System.Windows.Forms.ListView();
+            this.listViewKeukenBarOpmerkingen = new System.Windows.Forms.ListView();
             this.gereedKeukenBarBtn = new System.Windows.Forms.Button();
             this.annulerenKeukenBarBtn = new System.Windows.Forms.Button();
+            this.openstaandeBestellingenTxtLbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // terugKeukenBarBtn
             // 
-            this.terugKeukenBarBtn.Location = new System.Drawing.Point(13, 12);
+            this.terugKeukenBarBtn.BackColor = System.Drawing.Color.LightCoral;
+            this.terugKeukenBarBtn.Location = new System.Drawing.Point(12, 12);
             this.terugKeukenBarBtn.Name = "terugKeukenBarBtn";
             this.terugKeukenBarBtn.Size = new System.Drawing.Size(100, 36);
             this.terugKeukenBarBtn.TabIndex = 0;
             this.terugKeukenBarBtn.Text = "Terug naar home menu";
-            this.terugKeukenBarBtn.UseVisualStyleBackColor = true;
+            this.terugKeukenBarBtn.UseVisualStyleBackColor = false;
             this.terugKeukenBarBtn.Click += new System.EventHandler(this.terugKeukenBarBtn_Click);
             // 
             // listViewKeukenBarOpenstaand
@@ -55,49 +57,62 @@ namespace ChapooUI
             this.listViewKeukenBarOpenstaand.TabIndex = 1;
             this.listViewKeukenBarOpenstaand.UseCompatibleStateImageBehavior = false;
             // 
-            // listViewKeukenBarIngredientenOpmerkingen
+            // listViewKeukenBarOpmerkingen
             // 
-            this.listViewKeukenBarIngredientenOpmerkingen.HideSelection = false;
-            this.listViewKeukenBarIngredientenOpmerkingen.Location = new System.Drawing.Point(456, 77);
-            this.listViewKeukenBarIngredientenOpmerkingen.Name = "listViewKeukenBarIngredientenOpmerkingen";
-            this.listViewKeukenBarIngredientenOpmerkingen.Size = new System.Drawing.Size(317, 220);
-            this.listViewKeukenBarIngredientenOpmerkingen.TabIndex = 2;
-            this.listViewKeukenBarIngredientenOpmerkingen.UseCompatibleStateImageBehavior = false;
+            this.listViewKeukenBarOpmerkingen.HideSelection = false;
+            this.listViewKeukenBarOpmerkingen.Location = new System.Drawing.Point(456, 77);
+            this.listViewKeukenBarOpmerkingen.Name = "listViewKeukenBarOpmerkingen";
+            this.listViewKeukenBarOpmerkingen.Size = new System.Drawing.Size(317, 220);
+            this.listViewKeukenBarOpmerkingen.TabIndex = 2;
+            this.listViewKeukenBarOpmerkingen.UseCompatibleStateImageBehavior = false;
             // 
             // gereedKeukenBarBtn
             // 
+            this.gereedKeukenBarBtn.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.gereedKeukenBarBtn.Location = new System.Drawing.Point(456, 355);
             this.gereedKeukenBarBtn.Name = "gereedKeukenBarBtn";
             this.gereedKeukenBarBtn.Size = new System.Drawing.Size(141, 50);
             this.gereedKeukenBarBtn.TabIndex = 3;
             this.gereedKeukenBarBtn.Text = "Gereed";
-            this.gereedKeukenBarBtn.UseVisualStyleBackColor = true;
+            this.gereedKeukenBarBtn.UseVisualStyleBackColor = false;
             this.gereedKeukenBarBtn.Click += new System.EventHandler(this.gereedKeukenBarBtn_Click);
             // 
             // annulerenKeukenBarBtn
             // 
+            this.annulerenKeukenBarBtn.BackColor = System.Drawing.Color.LightCoral;
             this.annulerenKeukenBarBtn.Location = new System.Drawing.Point(632, 355);
             this.annulerenKeukenBarBtn.Name = "annulerenKeukenBarBtn";
             this.annulerenKeukenBarBtn.Size = new System.Drawing.Size(141, 50);
             this.annulerenKeukenBarBtn.TabIndex = 4;
             this.annulerenKeukenBarBtn.Text = "Annuleren";
-            this.annulerenKeukenBarBtn.UseVisualStyleBackColor = true;
+            this.annulerenKeukenBarBtn.UseVisualStyleBackColor = false;
             this.annulerenKeukenBarBtn.Click += new System.EventHandler(this.annulerenKeukenBarBtn_Click);
             // 
-            // KeukenSysteemForm
+            // openstaandeBestellingenTxtLbl
+            // 
+            this.openstaandeBestellingenTxtLbl.AutoSize = true;
+            this.openstaandeBestellingenTxtLbl.Location = new System.Drawing.Point(22, 61);
+            this.openstaandeBestellingenTxtLbl.Name = "openstaandeBestellingenTxtLbl";
+            this.openstaandeBestellingenTxtLbl.Size = new System.Drawing.Size(130, 13);
+            this.openstaandeBestellingenTxtLbl.TabIndex = 5;
+            this.openstaandeBestellingenTxtLbl.Text = "Openstaande bestellingen";
+            // 
+            // KeukenBarSysteemForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.openstaandeBestellingenTxtLbl);
             this.Controls.Add(this.annulerenKeukenBarBtn);
             this.Controls.Add(this.gereedKeukenBarBtn);
-            this.Controls.Add(this.listViewKeukenBarIngredientenOpmerkingen);
+            this.Controls.Add(this.listViewKeukenBarOpmerkingen);
             this.Controls.Add(this.listViewKeukenBarOpenstaand);
             this.Controls.Add(this.terugKeukenBarBtn);
-            this.Name = "KeukenSysteemForm";
+            this.Name = "KeukenBarSysteemForm";
             this.Text = "KeukenSysteemForm";
             this.Load += new System.EventHandler(this.KeukenSysteemForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -105,8 +120,9 @@ namespace ChapooUI
 
         private System.Windows.Forms.Button terugKeukenBarBtn;
         private System.Windows.Forms.ListView listViewKeukenBarOpenstaand;
-        private System.Windows.Forms.ListView listViewKeukenBarIngredientenOpmerkingen;
+        private System.Windows.Forms.ListView listViewKeukenBarOpmerkingen;
         private System.Windows.Forms.Button gereedKeukenBarBtn;
         private System.Windows.Forms.Button annulerenKeukenBarBtn;
+        private System.Windows.Forms.Label openstaandeBestellingenTxtLbl;
     }
 }
