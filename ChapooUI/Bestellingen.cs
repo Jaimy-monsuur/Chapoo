5e3d127d704c58ac5432b7e7ac1dc0eb0967f971
666987 <70690069+666987@users.noreply.github.com>
2021-05-27 13:42:33 +0200

    Create Bestellingen.cs

---
+++ ChapooUI/Bestellingen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChapooUI
+{
+    public partial class Bestellingen : Form
+    {
+        public Bestellingen()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Uitlog_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            ConfirmLogout confirmLogout = new ConfirmLogout();
+            confirmLogout.ShowDialog();
+        }
+
+        private void LBL_klok_Click(object sender, EventArgs e)
+        {
+            LBL_klok.Text = DateTime.Now.ToString(("HH:mm:ss"));
+        }
+    }
+}
